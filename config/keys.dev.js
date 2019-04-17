0605b87c44af609d2a1dae43e24012f32adfed92
mtleinon <mika.t.leinonen@gmail.com>
2019-04-18 02:08:47 +0300

    Added own configuration file for production environment.

---
+++ config/keys.dev.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://merntraining:merntraining@cluster0-ipmon.mongodb.net/mern?retryWrites=true',
+  jsonWebTokenKey: 'secret+1'
+};
+
+// module.exports = {
+//   mongoURI:
+//     "mongodb+srv://merntraining:merntraining@cluster0-ipmon.mongodb.net/mern?retryWrites=true",
+//   jsonWebTokenKey: "secret+1"
+// };
