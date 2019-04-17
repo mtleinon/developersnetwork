b7e9d9d1f4bb71486aa3d6d19d14a0591c5a248f
mtleinon <mika.t.leinonen@gmail.com>
2019-04-18 02:33:43 +0300

    Prepared for deploy

--- server.js
+++ server.js
@@ -1,41 +1,48 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const morgan = require("morgan");
-const users = require("./routes/api/users");
-const profile = require("./routes/api/profile");
-const posts = require("./routes/api/posts");
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 const app = express();
-const bodyParser = require("body-parser");
-const passport = require("passport");
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 // DB config
-const dbURI = require("./config/keys").mongoURI;
-console.log("dbURI", dbURI);
+const dbURI = require('./config/keys').mongoURI;
+console.log('dbURI', dbURI);
 mongoose
   .connect(dbURI)
-  .then(() => console.log("Connected to database"))
-  .catch(err => console.log("CATCH: db", err));
+  .then(() => console.log('Connected to database'))
+  .catch(err => console.log('CATCH: db', err));
 
 //morgan("tiny");
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use("/", (req, res, next) => {
+app.use('/', (req, res, next) => {
   console.log(req.method, req.url, req.body);
   next();
 });
 
 // passport middleware is used for checking JSON web token
 app.use(passport.initialize());
-require("./config/passport")(passport);
+require('./config/passport')(passport);
 
 // Routes
-app.get("/", (req, res) => res.send("Hello !!!"));
-app.use("/api/users", users);
-app.use("/api/profile", profile);
-app.use("/api/posts", posts);
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+// Server static assets if in production (React client)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log("Listening port ", port));
+app.listen(port, () => console.log('Listening port ', port));
