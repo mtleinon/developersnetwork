67ec5a0554deb58de8f89c54892e7dd4eb26e21a
mtleinon <mika.t.leinonen@gmail.com>
2020-03-01 01:09:05 +0200

    Changed class based components to functional components.

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,92 +1,74 @@
-import React, { Component } from 'react';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+
 import { getCurrentProfile, deleteAccount } from '../../actions/profileActions';
 import Spinner from '../common/Spinner';
 import ProfileActions from './ProfileActions';
 import Experience from './Experience';
 import Education from './Education';
 
-export class Dashboard extends Component {
-  componentDidMount() {
-    this.props.getCurrentProfile();
-  }
-  onDeleteClick = () => {
-    this.props.deleteAccount();
+export default function Dashboard() {
+  const dispatch = useDispatch();
+  const auth = useSelector(state => state.auth);
+  const currentProfile = useSelector(state => state.profile);
+
+  useEffect(() => {
+    dispatch(getCurrentProfile())
+  }, [dispatch])
+
+  const onDeleteClick = () => {
+    dispatch(deleteAccount());
   };
-  render() {
-    const { user } = this.props.auth;
-    const { profile, loading } = this.props.profile;
 
-    let dashboardContent;
+  const { user } = auth;
+  const { profile, loading } = currentProfile;
 
-    if (profile === null || loading) {
-      dashboardContent = <Spinner />;
-    } else {
-      // If user has profile, display it
-      if (Object.keys(profile).length > 0) {
-        dashboardContent = (
-          <div>
-            <p className="lead text-muted">
-              Welcome
+  let dashboardContent;
+
+  if (profile === null || loading) {
+    dashboardContent = <Spinner />;
+  } else {
+    // If user has profile, display it
+    if (Object.keys(profile).length > 0) {
+      dashboardContent = (
+        <div>
+          <p className="lead text-muted">
+            Welcome
               <Link to={`/profile/${profile.handle}`}> {user.name}</Link>
-            </p>
-            <ProfileActions />
-            <Experience experience={profile.experience} />
-            <Education education={profile.education} />
-            <div style={{ marginBottom: '60px' }} />
-            <button onClick={this.onDeleteClick} className="btn btn-danger">
-              Delete my account
+          </p>
+          <ProfileActions />
+          <Experience experience={profile.experience} />
+          <Education education={profile.education} />
+          <div style={{ marginBottom: '60px' }} />
+          <button onClick={onDeleteClick} className="btn btn-danger">
+            Delete my account
             </button>
-          </div>
-        );
-      } else {
-        // User doesn't have profile, ask to create one
-        dashboardContent = (
-          <div>
-            <p className="lead text-muted">Welcome {user.name}</p>
-            <Link to="/create-profile" className="btn btn-lg btn-info">
-              Create Profile
+        </div>
+      );
+    } else {
+      // User doesn't have profile, ask to create one
+      dashboardContent = (
+        <div>
+          <p className="lead text-muted">Welcome {user.name}</p>
+          <Link to="/create-profile" className="btn btn-lg btn-info">
+            Create Profile
             </Link>
-          </div>
-        );
-      }
+        </div>
+      );
     }
+  }
 
-    return (
-      <div className="dashboard">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12">
-              <h1>Dashboard</h1>
-              <h1 className="display-4">{dashboardContent}</h1>
-            </div>
+  return (
+    <div className="dashboard">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h1>Dashboard</h1>
+            <h1 className="display-4">{dashboardContent}</h1>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-Dashboard.propTypes = {
-  getCurrentProfile: PropTypes.func.isRequired,
-  deleteAccount: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired,
-  profile: PropTypes.object.isRequired
-};
-
-const mapStateToProps = state => ({
-  profile: state.profile,
-  auth: state.auth
-});
-
-const mapDispatchToProps = {
-  getCurrentProfile,
-  deleteAccount
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Dashboard);
