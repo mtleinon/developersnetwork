77e7989b8c21e5bf281c726abd3cdc7cac6129a6
mtleinon <mika.t.leinonen@gmail.com>
2020-03-01 00:19:59 +0200

    Converted state component to a functional component

--- client/src/components/add-credentials/AddExperience.js
+++ client/src/components/add-credentials/AddExperience.js
@@ -1,16 +1,19 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { Link, withRouter } from 'react-router-dom';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from "react-router-dom";
+
+import { Link } from 'react-router-dom';
 import TextFieldGroup from '../common/TextFieldGroup';
 import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
 import { addExperience } from '../../actions/profileActions';
 
-export class AddExperience extends Component {
-  constructor(props) {
-    super(props);
+export default function AddExperience() {
+  const history = useHistory();
+  const errors = useSelector(state => state.errors);
+  const dispatch = useDispatch();
 
-    this.state = {
+  const [state, setState] = useState(
+    {
       company: '',
       title: '',
       location: '',
@@ -20,149 +23,113 @@ export class AddExperience extends Component {
       description: '',
       errors: {},
       disabled: false
-    };
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.errors) {
-      this.setState({ errors: nextProps.errors });
-    }
-  }
-
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
-  onCheck = e => {
-    this.setState({
-      disabled: !this.state.disabled,
-      current: !this.state.current
     });
+
+  const onChange = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setState(s => ({ ...s, [name]: value }));
   };
 
-  onSubmit = e => {
+  const onCheck = e => {
+    setState(currentState => ({
+      ...currentState,
+      disabled: !currentState.disabled,
+      current: !currentState.current
+    }));
+  };
+
+  const onSubmit = e => {
     e.preventDefault();
 
-    const experienceData = {
-      company: this.state.company,
-      title: this.state.title,
-      location: this.state.location,
-      from: this.state.from,
-      to: this.state.to,
-      current: this.state.current,
-      description: this.state.description
-    };
-    if (experienceData.current === '') {
-      experienceData.current = 'false';
-    }
-
-    this.props.addExperience(experienceData, this.props.history);
+    dispatch(addExperience({
+      ...state,
+      current: state.current === '' ? 'false' : 'true'
+    },
+      history));
   };
 
-  render() {
-    const { errors } = this.state;
-    return (
-      <div className="add-experience">
-        <div className="container">
-          <div className="col-md-8 m-auto">
-            <Link to="/dashboard" className="btn btn-light">
-              Go Back
+  return (
+    <div className="add-experience">
+      <div className="container">
+        <div className="col-md-8 m-auto">
+          <Link to="/dashboard" className="btn btn-light">
+            Go Back
             </Link>
-            <h1 className="display-4 text-center">Add Experience</h1>
-            <p className="lead text-center">
-              Add any job or position that you have had in the past or currently
-              have.
+          <h1 className="display-4 text-center">Add Experience</h1>
+          <p className="lead text-center">
+            Add any job or position that you have had in the past or currently
+            have.
             </p>
-            <small className="d-block pb-3">* = required fields</small>
-            <form onSubmit={this.onSubmit}>
-              <TextFieldGroup
-                name="title"
-                placeHolder="* Job Title"
-                value={this.state.title}
-                onChange={this.onChange}
-                error={errors.title}
+          <small className="d-block pb-3">* = required fields</small>
+          <form onSubmit={onSubmit}>
+            <TextFieldGroup
+              name="title"
+              placeHolder="* Job Title"
+              value={state.title}
+              onChange={onChange}
+              error={errors.title}
+            />
+            <TextFieldGroup
+              name="company"
+              placeHolder="* Company"
+              value={state.company}
+              onChange={onChange}
+              error={errors.company}
+            />
+            <TextFieldGroup
+              name="location"
+              placeHolder="Location"
+              value={state.location}
+              onChange={onChange}
+              error={errors.location}
+            />
+            <h6>From date</h6>
+            <TextFieldGroup
+              name="from"
+              type="date"
+              value={state.from}
+              onChange={onChange}
+              error={errors.from}
+            />
+            <h6>To date</h6>
+            <TextFieldGroup
+              name="to"
+              type="date"
+              value={state.to}
+              onChange={onChange}
+              error={errors.to}
+              disabled={state.disabled ? 'disabled' : ''}
+            />
+            <div
+              className="from-check md-4"
+              style={{ padding: '0 0 20px 20px', marginTop: '-10px' }}
+            >
+              <input
+                type="checkbox"
+                className="form-check-input"
+                name="current"
+                value={state.current}
+                checked={state.current}
+                onChange={onCheck}
+                id="current"
               />
-              <TextFieldGroup
-                name="company"
-                placeHolder="* Company"
-                value={this.state.company}
-                onChange={this.onChange}
-                error={errors.company}
-              />
-              <TextFieldGroup
-                name="location"
-                placeHolder="Location"
-                value={this.state.location}
-                onChange={this.onChange}
-                error={errors.location}
-              />
-              <h6>From date</h6>
-              <TextFieldGroup
-                name="from"
-                type="date"
-                value={this.state.from}
-                onChange={this.onChange}
-                error={errors.from}
-              />
-              <h6>To date</h6>
-              <TextFieldGroup
-                name="to"
-                type="date"
-                value={this.state.to}
-                onChange={this.onChange}
-                error={errors.to}
-                disabled={this.state.disabled ? 'disabled' : ''}
-              />
-              <div
-                className="from-check md-4"
-                style={{ padding: '0 0 20px 20px', marginTop: '-10px' }}
-              >
-                <input
-                  type="checkbox"
-                  className="form-check-input"
-                  name="current"
-                  value={this.state.current}
-                  checked={this.state.current}
-                  onChange={this.onCheck}
-                  id="current"
-                />
-                <label htmlFor="current" className="form-check-label">
-                  Current Job
+              <label htmlFor="current" className="form-check-label">
+                Current Job
                 </label>
-              </div>
-              <TextAreaFieldGroup
-                name="description"
-                placeHolder="Job description"
-                value={this.state.description}
-                onChange={this.onChange}
-                error={errors.description}
-                info="Tell us about the position"
-              />
-              <input type="submit" className="btn btn-info btn-block mt-4" />
-            </form>
-          </div>
+            </div>
+            <TextAreaFieldGroup
+              name="description"
+              placeHolder="Job description"
+              value={state.description}
+              onChange={onChange}
+              error={errors.description}
+              info="Tell us about the position"
+            />
+            <input type="submit" className="btn btn-info btn-block mt-4" />
+          </form>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-AddExperience.propTypes = {
-  errors: PropTypes.object.isRequired,
-  profile: PropTypes.object.isRequired,
-  addExperience: PropTypes.func.isRequired
-};
-
-const mapStateToProps = state => ({
-  errors: state.errors,
-  profile: state.profile
-});
-
-const mapDispatchToProps = {
-  addExperience
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(withRouter(AddExperience));
